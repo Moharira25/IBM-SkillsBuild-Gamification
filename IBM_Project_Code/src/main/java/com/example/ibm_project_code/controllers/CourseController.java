c999cb49bb1c89d36d0c3d27118c9dc118d46509
Mohammed Abuharira <maya2@student.le.ac.uk>
2024-02-06 18:05:43 +0000

    Created a controller and a view for dashboard and course starting.

---
+++ IBM_Project_Code/src/main/java/com/example/ibm_project_code/controllers/CourseController.java
@@ -0,0 +1,55 @@
+package com.example.ibm_project_code.controllers;
+
+import com.example.ibm_project_code.database.Course;
+import com.example.ibm_project_code.database.User;
+import com.example.ibm_project_code.repositories.CourseRepository;
+import com.example.ibm_project_code.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.*;
+
+@Controller
+public class CourseController {
+    @Autowired
+    private CourseRepository courseRepo;
+    @Autowired
+    private UserRepository userRepo;
+
+
+
+    @GetMapping("/dashboard")
+    public String list(Model model, @RequestParam Long userId) {
+        Map<String, List<Course>> coursesByCategory = new HashMap<>();
+        for (Course course : courseRepo.findAll()) {
+            String category = course.getCategory();
+
+            //adding a category-courses pair if they are not present in the hashmap
+            coursesByCategory.putIfAbsent(category, new ArrayList<>());
+
+            // Add the course to the list from the pair above
+            coursesByCategory.get(category).add(course);
+        }
+        model.addAttribute("coursesByCategory", coursesByCategory);
+        model.addAttribute("userId", userId);
+        return "dashboard";
+    }
+
+    @RequestMapping("/start/{userId}/{courseId}")
+    public String startLearning(Model model, @PathVariable Long userId, @PathVariable int courseId) {
+        Course course = courseRepo.findById(courseId);
+        Optional<User> user = userRepo.findById(userId);
+        if (!user.isEmpty()) {
+            user.get().getCourses().add(course);
+            course.getUsers().add(user.get());
+            userRepo.save(user.get());
+            courseRepo.save(course);
+        }
+        return "redirect:"+course.getLink();
+    }
+}
