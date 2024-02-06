b49fbe5c1844ff944392f1637023e610e2a20ca9
Mohammed Abuharira <maya2@student.le.ac.uk>
2024-02-06 18:01:53 +0000

    Implemented scrapping logic in class Application.java and saved data to repository.

--- IBM_Project_Code/src/main/java/com/example/ibm_project_code/Application.java
+++ IBM_Project_Code/src/main/java/com/example/ibm_project_code/Application.java
@@ -1,7 +1,13 @@
 package com.example.ibm_project_code;
 
+import com.example.ibm_project_code.database.Course;
+import com.example.ibm_project_code.repositories.CourseRepository;
 import com.example.ibm_project_code.services.CustomUserDetailsService;
 import io.github.cdimascio.dotenv.Dotenv;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -12,6 +18,8 @@ public class Application implements CommandLineRunner {
 
     @Autowired
     private CustomUserDetailsService userDetailsService;
+    @Autowired
+    private CourseRepository courseRepository;
 
     // loads .env data correctly
     static {
@@ -27,5 +35,35 @@ public class Application implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         userDetailsService.addTestUser();
+
+        //scrapping course-data (college students pathway only) from IBM skills build website.
+        Document doc = Jsoup
+                .connect("https://skillsbuild.org/college-students/digital-credentials")
+                .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36")
+                .get();
+        Elements categories = doc.getElementsByClass("pb-12");
+
+        for (Element c : categories) {
+            Element category = c.getElementsByClass("badge-section-title bx--productive-heading-06 pt-8 pb-12 max-w-9/10").getFirst();
+            Elements categoryElements = c.getElementsByClass("mb-16 bx--row");
+            for (Element cE : categoryElements) {
+
+                Course course = new Course();
+                course.setCategory(category.text());
+                Element title = cE.selectFirst(".bx--expressive-heading-03.mb-4");
+                Element language = cE.select(".ml-1").get(0);
+                Element duration = cE.select(".ml-1").get(2);
+                Element description = cE.getElementsByClass("bx--body-long-02 max-w-9/10").select("p").getFirst();
+                String link = cE.getElementsByClass("flex flex-col md:flex-row").select("a").getLast().attr("href");
+
+                course.setTitle(title.text());
+                course.setDuration(duration.text());
+                course.setLanguage(language.text());
+                course.setDescription(description.text());
+                course.setLink(link);
+                courseRepository.save(course);
+            }
+        }
+
     }
 }
