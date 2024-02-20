34389c82542e3070aea5287db210cf79732ae06c
Mohammed Abuharira <maya2@student.le.ac.uk>
2024-02-20 01:42:24 +0000

    Adjusted leaderboard to highlight current user

--- IBM_Project_Code/src/main/java/com/example/ibm_project_code/controllers/LeaderboardController.java
+++ IBM_Project_Code/src/main/java/com/example/ibm_project_code/controllers/LeaderboardController.java
@@ -3,6 +3,8 @@ package com.example.ibm_project_code.controllers;
 import com.example.ibm_project_code.database.User;
 import com.example.ibm_project_code.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,7 +27,17 @@ public class LeaderboardController {
                 return o2.getOverallPoints() - o1.getOverallPoints();
             }
         });
+        User user = userAuth();
+        Long id = user != null ? user.getId():1;
+        model.addAttribute("userId", id);
         model.addAttribute("users", users);
         return "leaderboard";
+    }
+
+    private User userAuth() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String username = auth.getName(); // Get the username
+        // Retrieve the user by username
+        return userRepo.findByUsername(username).orElse(null);
     }
 }
