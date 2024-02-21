d297c985a3aa0cf4cf930d12c3b810bc75a7d1ee
Mohammed Abuharira <maya2@student.le.ac.uk>
2024-02-21 04:17:15 +0000

    Added LandingPageController.java to make navbar responsive in landing.html

---
+++ IBM_Project_Code/src/main/java/com/example/ibm_project_code/controllers/LandingPageController.java
@@ -0,0 +1,33 @@
+package com.example.ibm_project_code.controllers;
+
+import com.example.ibm_project_code.database.User;
+import com.example.ibm_project_code.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class LandingPageController {
+    @Autowired
+    private UserRepository userRepo;
+
+    @GetMapping("/")
+    public String leaderboard(Model model) {
+
+        User user = userAuth();
+        Long id = user != null ? user.getId():1;
+        model.addAttribute("userId", id);
+        return "landing";
+    }
+
+    private User userAuth() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String username = auth.getName(); // Get the username
+        // Retrieve the user by username
+        return userRepo.findByUsername(username).orElse(null);
+    }
+}
